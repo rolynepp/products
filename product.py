d1fcdcff3a85d40ba662d851f0ad539a2665f012
rolyne chen <rolynepp@gmail.com>
2018-09-17 11:25:36 +0800

    csv write

--- product.py
+++ product.py
@@ -15,6 +15,7 @@ print(products)
 for p in products:
 	print(p[0], '的價格是', p[1])
 
-with open('products.txt', 'w') as f:
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ','+ str(p[1]) + '\n')
