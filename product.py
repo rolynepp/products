961f5dd816a808b59300d0c71cf867c4ce5a0c29
rolyne chen <rolynepp@gmail.com>
2018-09-17 15:43:16 +0800

    all file

--- product.py
+++ product.py
@@ -1,13 +1,14 @@
 #讀取檔案
 products = []
-with open('products.csv', 'r', encoding= 'utf-8') as f:
+with open('products.csv', 'r') as f:
 	for line in f:
+		if '商品,價格' in line:
+			continue
 		name, price = line.strip().split(',')
 		products.append([name, price])
-
 print(products)
 
-
+#讓使用者輸入
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q': #quit
