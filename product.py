a263fa89c634a1ff40adf3bbe69b438f8cd0fba9
rolyne chen <rolynepp@gmail.com>
2018-09-17 11:13:20 +0800

    str change

--- product.py
+++ product.py
@@ -4,6 +4,7 @@ while True:
 	if name == 'q': #quit
 		break
 	price = input('請輸入價格: ')
+	price = int(price)
 	#p = []
 	#p.append(name)
 	#p.append(price) 下方為清單簡化
@@ -16,4 +17,4 @@ for p in products:
 
 with open('products.txt', 'w') as f:
 	for p in products:
-		f.write(p[0] + ','+ p[1] + '\n')
+		f.write(p[0] + ','+ str(p[1]) + '\n')
